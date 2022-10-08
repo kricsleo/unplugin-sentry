5db516c1e47399780699fcdf8546cc7c098716c4
许圣峰 <kricsleo@163.com>
2022-10-08 13:50:06 +0800

    chore: init

---
+++ playground/vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import Inspect from 'vite-plugin-inspect'
+import Unplugin from '../src/vite'
+
+export default defineConfig({
+  plugins: [
+    Inspect(),
+    Unplugin(),
+  ],
+  build: {
+    sourcemap: true,
+  },
+})
